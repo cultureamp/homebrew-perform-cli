afbf6c93aa33c7a6c23a312b3ac2ee1aa2ca4cf4
Todd Moore <todd@toddm.me>
2019-09-05 14:10:59 +1000

    Update checksum

--- Formula/perform-cli.rb
+++ Formula/perform-cli.rb
@@ -2,7 +2,7 @@ class PerformCli < Formula
   desc "Performance Camp CLI for doing all the hings"
   homepage "https://github.com/cultureamp/perform-cli"
   url "https://github.com/cultureamp/perform-cli/archive/v0.0.1.tar.gz"
-  sha256 "9bb26e70a4245bdec426724b6e35a252ab841dd65cae8b200af5dced183283cf"
+  sha256 "7b9b4774e8b5f8884b3e3c73393156c3c45ebb6082e3ce9ecddaf595e99d3bea"
   head "https://github.com/cultureamp/perform-cli.git"
 
   bottle :unneeded
