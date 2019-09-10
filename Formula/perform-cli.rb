31e9348c14b9ad68480405d81f91c3f86637581f
Todd Moore <todd@toddm.me>
2019-09-10 16:36:54 +1000

    Use ssh url for formula

--- Formula/perform-cli.rb
+++ Formula/perform-cli.rb
@@ -3,7 +3,7 @@ class PerformCli < Formula
   homepage "https://github.com/cultureamp/perform-cli"
   url "https://github.com/cultureamp/perform-cli/archive/v0.0.1.tar.gz"
   sha256 "7b9b4774e8b5f8884b3e3c73393156c3c45ebb6082e3ce9ecddaf595e99d3bea"
-  head "https://github.com/cultureamp/perform-cli.git"
+  head "git@github.com:cultureamp/perform-cli.git", :using => :git
 
   bottle :unneeded
 
