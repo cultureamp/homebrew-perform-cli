e820611a233dbdf84e283ca7556b2a539001532f
Todd Moore <todd@toddm.me>
2019-09-12 12:34:09 +1000

    Update formula to use master branch from repo

--- Formula/perform-cli.rb
+++ Formula/perform-cli.rb
@@ -1,13 +1,17 @@
 class PerformCli < Formula
   desc "Performance Camp CLI for doing all the hings"
   homepage "https://github.com/cultureamp/perform-cli"
-  url "https://github.com/cultureamp/perform-cli/archive/v0.0.1.tar.gz"
-  sha256 "7b9b4774e8b5f8884b3e3c73393156c3c45ebb6082e3ce9ecddaf595e99d3bea"
+  url "git@github.com:cultureamp/perform-cli.git", :using => :git
   head "git@github.com:cultureamp/perform-cli.git", :using => :git
 
   bottle :unneeded
 
   depends_on "jq"
+
+  devel do
+    url "git@github.com:cultureamp/perform-cli.git", :using => :git, :tag => "v0.0.2"
+    depends_on "jq"
+  end
 
   def install
     system "./install.bash", prefix
