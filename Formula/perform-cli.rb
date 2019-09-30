d0b3378cd5a5ec1543b4a714c026565077add89e
Todd Moore <todd@toddm.me>
2019-09-30 17:45:25 +1000

    Updated formula to v1.0.0 release

--- Formula/perform-cli.rb
+++ Formula/perform-cli.rb
@@ -1,15 +1,14 @@
 class PerformCli < Formula
   desc "Performance Camp CLI for doing all the hings"
   homepage "https://github.com/cultureamp/perform-cli"
-  url "git@github.com:cultureamp/perform-cli.git", :using => :git
-  head "git@github.com:cultureamp/perform-cli.git", :using => :git
+  url "git@github.com:cultureamp/perform-cli.git", :using => :git, :tag => "v1.0.0"
 
   bottle :unneeded
 
   depends_on "jq"
 
   devel do
-    url "git@github.com:cultureamp/perform-cli.git", :using => :git, :tag => "v0.0.2"
+    url "git@github.com:cultureamp/perform-cli.git", :using => :git, :tag => "v1.0.0"
     depends_on "jq"
   end
 
