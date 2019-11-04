56e4ed72e6bcc7b3491c7b992295de2f9342ee65
Todd Moore <todd@toddm.me>
2019-11-04 15:10:59 +1100

    Add 3.1.1 devel update

--- Formula/perform-cli.rb
+++ Formula/perform-cli.rb
@@ -1,18 +1,20 @@
+# frozen_string_literal: true
+
 class PerformCli < Formula
-  desc "Performance Camp CLI for doing all the hings"
-  homepage "https://github.com/cultureamp/perform-cli"
-  url "git@github.com:cultureamp/perform-cli.git", :using => :git, :tag => "v3.1.0"
+  desc 'Performance Camp CLI for doing all the hings'
+  homepage 'https://github.com/cultureamp/perform-cli'
+  url 'git@github.com:cultureamp/perform-cli.git', using: :git, tag: 'v3.1.0'
 
   bottle :unneeded
 
-  depends_on "jq"
+  depends_on 'jq'
 
   devel do
-    url "git@github.com:cultureamp/perform-cli.git", :using => :git, :tag => "v3.1.0"
-    depends_on "jq"
+    url 'git@github.com:cultureamp/perform-cli.git', using: :git, tag: 'v3.1.1'
+    depends_on 'jq'
   end
 
   def install
-    system "./install.bash", prefix
+    system './install.bash', prefix
   end
 end
