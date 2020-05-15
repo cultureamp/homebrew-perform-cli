b587ff3dbd7c66773f1a8b9d8d1c3c398327a016
Todd Moore <todd.moore@cultureamp.com>
2020-05-15 12:21:05 +1000

    Fix non saved perform-cli

--- Formula/perform-cli.rb
+++ Formula/perform-cli.rb
@@ -3,11 +3,7 @@
 class PerformCli < Formula
   desc 'Performance Camp CLI for doing all the hings'
   homepage 'https://github.com/cultureamp/perform-cli'
-<<<<<<< HEAD
   url 'git@github.com:cultureamp/perform-cli.git', using: :git, tag: 'v6.0.0'
-=======
-  url 'git@github.com:cultureamp/perform-cli.git', using: :git, tag: 'v5.2.0'
->>>>>>> a6dd3f17465b904958a73a8bd40df72abe3bf03a
 
   bottle :unneeded
 
