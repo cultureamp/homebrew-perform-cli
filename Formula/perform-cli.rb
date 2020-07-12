1bfd217b1ffb357addf94c455b06b0c08b17ab67
goreleaserbot <goreleaser@carlosbecker.com>
2020-07-12 20:39:50 +1000

    Brew formula update for perform-cli version v10.6.4

--- Formula/perform-cli.rb
+++ Formula/perform-cli.rb
@@ -3,16 +3,16 @@ require_relative "../custom_download_strategy"
 class PerformCli < Formula
   desc ""
   homepage "https://cultureamp.github.io/perform-cli/"
-  version "10.6.0"
+  version "10.6.4"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/cultureamp/perform-cli/releases/download/v10.6.0/perform-cli_10.6.0_Darwin_x86_64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "2db072dd91a329d849985791fb698dc9c68f0763dd0df9bad83cd8573e4b7958"
+    url "https://github.com/cultureamp/perform-cli/releases/download/v10.6.4/perform-cli_10.6.4_Darwin_x86_64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    sha256 "281077614f68de8d8235e17d0e3de021467e82f918cffa045fc9ab3841207276"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/cultureamp/perform-cli/releases/download/v10.6.0/perform-cli_10.6.0_Linux_x86_64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-      sha256 "70f6e64100c755cc5467c1d0c8f42f59a4c6c98701312d99707936c08dd807da"
+      url "https://github.com/cultureamp/perform-cli/releases/download/v10.6.4/perform-cli_10.6.4_Linux_x86_64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+      sha256 "6dbe94e0cb69045021bed7b1f929c036cc90d54eac09e4a134de117214abad2d"
     end
   end
 
