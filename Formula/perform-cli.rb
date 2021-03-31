82d8f3b03da4c3864ea77168bdefb759b49c5a91
goreleaserbot <goreleaser@carlosbecker.com>
2021-04-01 10:43:24 +1100

    Brew formula update for perform-cli version v10.11.11

--- Formula/perform-cli.rb
+++ Formula/perform-cli.rb
@@ -6,25 +6,23 @@ require_relative "../custom_download_strategy"
 class PerformCli < Formula
   desc ""
   homepage "https://cultureamp.github.io/perform-cli/"
-  version "10.11.10"
+  version "10.11.11"
   bottle :unneeded
 
-  if OS.mac?
-    url "https://github.com/cultureamp/perform-cli/releases/download/v10.11.10/perform-cli_10.11.10_Darwin_x86_64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "8cae4355483628e557628333e2bfde14300fb38e91328ea0f5be4a73fe27bff3"
+  if OS.mac? && Hardware::CPU.intel?
+    url "https://github.com/cultureamp/perform-cli/releases/download/v10.11.11/perform-cli_10.11.11_Darwin_x86_64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    sha256 "59266970058a847beb225c260e70c9d114dbba8b5e1080a691e4fccf1096ae76"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/cultureamp/perform-cli/releases/download/v10.11.10/perform-cli_10.11.10_Linux_x86_64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "dc2baa61503a0a3a20597557213f73d977de8faacf29bc0788d1d669f04dfc41"
+    url "https://github.com/cultureamp/perform-cli/releases/download/v10.11.11/perform-cli_10.11.11_Linux_x86_64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    sha256 "764863ca75fe88d44674edff02189af58fb2862c14fa627d82f840e7811d9476"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/cultureamp/perform-cli/releases/download/v10.11.10/perform-cli_10.11.10_Linux_arm64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "f9ba9fd40ccccd78e7aa778e368b2631e38e7c12859680a235aa2aca0c7654c0"
+    url "https://github.com/cultureamp/perform-cli/releases/download/v10.11.11/perform-cli_10.11.11_Linux_arm64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    sha256 "6cae910dd1888dc30ca327ca3724abe986f636c6c7ee4477f4acb50b8b309f30"
   end
 
   def install
     bin.install "perform-cli"
-    bin.install "perform-cli-logs"
-    bin.install "perform-cli-status"
   end
 end
