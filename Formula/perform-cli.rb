c2993fc34269ca65e6afacdcf31e76ced22c9867
goreleaserbot <goreleaser@carlosbecker.com>
2021-04-16 15:54:15 +1000

    Brew formula update for perform-cli version v10.11.13

--- Formula/perform-cli.rb
+++ Formula/perform-cli.rb
@@ -6,20 +6,20 @@ require_relative "../custom_download_strategy"
 class PerformCli < Formula
   desc ""
   homepage "https://cultureamp.github.io/perform-cli/"
-  version "10.11.12"
+  version "10.11.13"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/cultureamp/perform-cli/releases/download/v10.11.12/perform-cli_10.11.12_Darwin_x86_64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "3dfcf478f965518f8ffa8f824ee40e36361a7751186370784338676740e2022f"
+    url "https://github.com/cultureamp/perform-cli/releases/download/v10.11.13/perform-cli_10.11.13_Darwin_x86_64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    sha256 "9c4bd2abffb2beaab6ffda7a152c125a143926de6f71c4aa8d8b21c130ee2b11"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/cultureamp/perform-cli/releases/download/v10.11.12/perform-cli_10.11.12_Linux_x86_64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "536000fd17a50244f41ccbf3678e1247229fed303368a2c6834450a223cecdc3"
+    url "https://github.com/cultureamp/perform-cli/releases/download/v10.11.13/perform-cli_10.11.13_Linux_x86_64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    sha256 "856dc7fb3fade6e21af910bca9447c9e7c512200c70b837fe16f5ae0caf33285"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/cultureamp/perform-cli/releases/download/v10.11.12/perform-cli_10.11.12_Linux_arm64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "e372431a93f71241dab80d3632369e4c40e08579a39fa220d377263c63de96dd"
+    url "https://github.com/cultureamp/perform-cli/releases/download/v10.11.13/perform-cli_10.11.13_Linux_arm64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    sha256 "aba8c397ce3e92ca918dd5d2a7295bc316229bd409c3af55b7661069355825f2"
   end
 
   def install
